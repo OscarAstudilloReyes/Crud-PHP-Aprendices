14eda470652fb1c69249011710a48196227f942c
OscarAstudillo <osastudillor.iss@gmail.com>
2021-04-22 20:31:12 -0500

    incluir envio de correo electronico

--- Controlador/AprendizController.php
+++ Controlador/AprendizController.php
@@ -5,6 +5,7 @@ extract($_REQUEST);
 include "../Modelo/Aprendiz.php";
 include "../Modelo/Conexion.php";
 include "../Modelo/DatosAprendiz.php";
+include "../Modelo/enviarCorreo.php";
 // para evitar errores de tipo warning
 error_reporting(1);
 //Objeto del aprendiz
@@ -17,10 +18,37 @@ if ($accion == "Consultar") {
         $resultado = $aPrendiz->consultar($txtIdentificacion);
         echo  json_encode($resultado);
 } elseif ($accion == "Agregar") {
+        
         // METODO DE AGREGAR
         $ap = new Aprendiz($txtIdentificacion, $txtNombre, $txtApellido, $txtCorreo, $cbGenero, $fecha);
         $resultado = $aPrendiz->agregar($ap);
-        // print_r($resultado);
+        //Enviar correo cuando se inicia sesion
+        $correo = new enviarCorreoPrueba();
+        $objCorreo = new stdClass();
+        $objCorreo->correoRemitente = "soporte.petsHealth@gmail.com"; //aqui pueden colocar el correo del administrador
+        $objCorreo->nombreRemitente = "Administración ADSI"; //igual el nombre del administrador
+        $objCorreo->correoDestinatario =$txtCorreo;
+        $objCorreo->nombreDestinatario =$txtNombre;
+        $objCorreo->asunto = "Creacion de usuario en adsi";
+        $objCorreo->mensaje = "Cordial saludo , <br> Usuario " .$txtNombre."
+        " . " se ha creado su usuario correctamente
+        <br>
+        ¿fuiste tu ?
+        <table  width='50%' border='0' >
+        <tr>
+        <td width ='50%' align='center'>
+        <img src='https://i.imgur.com/yzjVfUS.png' alt='logoLargoEmpresa' width='250' >
+        </td>
+        <td width='50%'>
+        <br>
+        <b> Atentamente ADSI	</b>
+        <br>
+        Gracias por confiar en nosotros
+        </td>
+        </tr>
+        </table>";
+       $resultadoCorreo = $correo->enviarCorreo($objCorreo);
+       $resultado->mensaje = $resultado->mensaje . " " . $resultadoCorreo->mensaje;
         //Devueve el resultado en formatio json
         echo  json_encode($resultado);
 }
